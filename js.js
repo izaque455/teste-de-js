d43248f559b40b5d31583b3227701d98def8d134
izaque.455 <pereiraizaque407@gmail.com>
2025-07-22 09:49:08 -0300

    alteração 2

--- js.js
+++ js.js
@@ -1,19 +1,32 @@
-let num1 = 900
-let num2 = 900
-let num3 = 800
+let num1 = 600
+let num2 = 500
+let num3 = 600
+let resut = document.querySelector('h1')
 
 if(num1 === num2 && num1 === num3){
-    console.log('os numeros são todos iguais!')
+    resut.textContent = "todos são iguais"
+}
+
+else if (num1 === num2 && num2 >= num3 ) {
+    resut.textContent = 'o num1 ('+ num1+') e o num2 ('+num2+') é maior que o num3 que é ('+num3+')'
+}
+
+else if (num2 === num3 && num3 >= num1 ) {
+    resut.textContent = 'o num2 ('+ num2+') e o num3 ('+num3+') é maior que o num1 que é ('+num1+')' 
+}
+
+else if (num1 === num3 && num3 >= num2) {
+    resut.textContent = 'o num1('+ num1+') e o num3 ('+num3+') é maior que o num2 que é ('+num2+')' 
 }
 
 else if (num1 >= num2 && num1 >= num3){
-console.log("o num1 ("+ num1 +") é o maior")
+resut.textContent = "o num1 ("+ num1 +") é o maior"
 }
 
 else if(num2 >= num1 && num2 >= num3){
-    console.log('o num2 ('+ num2 +') é o maior')
+    resut.textContent = 'o num2 ('+ num2 +') é o maior'
 }
 
 else if(num3 >= num1 && num3 >= num2){
-    console.log ('num3 ('+num3+') é o maior')
+    resut.textContent = 'num3 ('+num3+') é o maior'
 }
