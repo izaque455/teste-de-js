30b632624c8d4533a831e5e368c304dd093ec211
izaque.455 <pereiraizaque407@gmail.com>
2025-08-03 14:12:58 -0300

    Jokempo

--- jokempo js/script.js
+++ jokempo js/script.js
@@ -1,14 +1,43 @@
-const playhuman = (humanChoice) => {
-    console.log(humanChoice)
+const resultado = document.querySelector('.result');
+const resultDaAlexa = document.querySelector('.result-da-alexa');
+const meuspontos = document.querySelector('.resultado-seu');
+const pontosdaAlexa = document.querySelector('.resultado-da-alexa');
+const resultseu = document.querySelector('.result-seu');
+let humansocorNumber = 0
+let machinesocorNumber = 0
 
-    console.log('retorno' + playhuman())
+const playHuman = (humanChoice) => {
+
+    playTheGame(humanChoice, playMachine())
 }
+const playMachine = () => {
+    const choices = ['pedra', 'papel', 'tesoura']
+    const ramdoNumber = Math.floor(Math.random() * 3)
 
-const playmaquina = () => {
-    const opçoes = ['pedra', 'papel', 'tesoura']
-    const randomNumber = Math.floor(Math.random() * 3)
 
-    console.log(randomNumber)
 
-    return opçoes[randomNumber]
+    return choices[ramdoNumber]
+}
+const playTheGame = (human, machine) => {
+    console.log('Humano: ' + human + " Maquina: " + machine)
+    resultDaAlexa.innerHTML = "Alexa escolheu: " + machine;
+    resultseu.innerHTML = "Voce escolheu: " + human;
+
+    if (human === machine) {
+        resultado.innerHTML = "Deu Empate!!"
+    }
+    else if (
+        (human === 'papel' && machine === 'pedra') ||
+        (human === 'pedra' && machine === 'tesoura') ||
+        (human === 'tesoura' && machine === 'papel')) {
+        humansocorNumber++;
+        meuspontos.innerHTML = humansocorNumber
+        resultado.innerHTML = "Você Ganhou!!"
+
+    }
+    else {
+        machinesocorNumber++;
+        pontosdaAlexa.innerHTML = machinesocorNumber
+        resultado.innerHTML = 'Você perdeu para alexa'
+    }
 }
